6d8b834a17394d631e1de2aad9595ad1521c5e16
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-09-17 20:29:28 +0200

    gitignore

--- node_modules/@react-native-community/cli-platform-android/build/link/warnAboutManuallyLinkedLibs.d.ts
+++
@@ -1,3 +0,0 @@
-import { Config } from '../types';
-export default function warnAboutManuallyLinkedLibs(config: Config, platform?: string, linkConfig?: ReturnType<Config['platforms']['android']['linkConfig']>): void;
-//# sourceMappingURL=warnAboutManuallyLinkedLibs.d.ts.map
